5df49c11a06aac636cc332795d10a5b42098d678
RonaldKemper <ronspoolco@gmail.com>
2021-10-31 19:26:38 -0400

    'revision'

--- script.js
+++ script.js
@@ -20,15 +20,25 @@ const toggleIcon = (e) => {
   $('.img3').on('mouseover', toggleIcon);
   $('.img3').on('mouseout', toggleIcon);
   
-function loadGithubRepos(){
-    let gitHubRequest = new XMLHttpRequest();
-    gitHubRequest.onreadystatechange = function() {
+function loadRepo() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        let gitObject = JSON.parse(this.responseText);
-        document.getElementById("githubRepos").innerHTML = this.responseText;
+      results = JSON.parse(this.responseText);
+      for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        let ul = document.getElementById("repositories");
+        let li = document.createElement("LI");
+        li.className = "list-group-item";
+        let a = document.createElement("a");
+        li.appendChild(document.createTextNode(results[i].name));
+        a.appendChild(li);
+        a.setAttribute("href", results[i].html_url);
+        ul.appendChild(a);
+	document.getElementById("githubRepos").innerHTML = this.responseText;
+      }
     }
-    };
-    gitHubRequest.open("GET", "https://api.github.com/users/RonnieKemper/repos", true);
-    gitHubRequest.send();
+  };
+  xhttp.open("GET", "https://api.github.com/users/RonnieKemper/repos", true);
+  xhttp.send();
 }
-loadGithubRepos()
